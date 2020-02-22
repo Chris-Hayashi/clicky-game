3e5ce717879abd5c2ce0932b68f9346a14406257
Christopher Hayashi <Chrisrhayashi@gmail.com>
2020-02-22 10:35:17 -0800

    Updates

---
+++ precache-manifest.fd0c907c1db097fdae50c83da5ca571e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b492256b0f3f238b67873fc0701f8f91",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "e2d691df11531cb3ecf0",
+    "url": "/clicky-game/static/css/main.ca1ac54c.chunk.css"
+  },
+  {
+    "revision": "84f7d0d1e41877910885",
+    "url": "/clicky-game/static/js/2.49f8e6b3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/clicky-game/static/js/2.49f8e6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d691df11531cb3ecf0",
+    "url": "/clicky-game/static/js/main.b570ee49.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  }
+]);
