84bd62897ca08ae0ec87e1a8a5eeadae0a09ec7f
Christopher Hayashi <Chrisrhayashi@gmail.com>
2020-02-22 01:52:32 -0800

    implemented Card/index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import Header from "./components/Header";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
 
 class App extends Component {
     render() {
-
+        return (
+            <Header />
+            
+        );
     }
 }
 
---
+++ client/src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Card = props => {
+    return(
+        <div className="card">
+            <img src={props.image} />
+        </div>
+    );
+}
+
+export default Card;
